e8301fe427df54db6636f8ab5beb144ba425d406
Felipe Mendes <zo.fmendes@gmail.com>
2021-07-19 05:04:25 -0300

    :clown_face: update text mock

--- src/compositions/about.tsx
+++ src/compositions/about.tsx
@@ -13,18 +13,25 @@ export const AboutComposition: React.FC = () => {
         better than yesterday.
       </p>
       <p>
-        A tech enthusiast since I was a kid, I started writing my first lines of
-        code around age 13, just for fun. Instigated by curiosity, I have
-        improved my taste for programming and development over the years. I keep
-        dedicating myself and studying more and more. Always looking to be
-        better than yesterday.
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce elementum
+        quam eget libero pretium cursus. Maecenas sed mi felis. Etiam eu ipsum
+        et libero tristique malesuada. Sed nulla neque, fringilla nec enim eget,
+        scelerisque feugiat est. Suspendisse potenti. Aliquam quis velit
+        imperdiet, feugiat mi non, cursus mi. Vestibulum ut interdum nisi, sed
+        scelerisque odio. Vestibulum venenatis pretium purus, eu venenatis
+        tortor mattis eu. Ut velit ex, tempor in neque et, volutpat sodales sem.
+        Integer luctus, dolor sed laoreet viverra, sapien ante tincidunt ex, eu
+        vulputate purus enim at odio.
       </p>
       <p>
-        A tech enthusiast since I was a kid, I started writing my first lines of
-        code around age 13, just for fun. Instigated by curiosity, I have
-        improved my taste for programming and development over the years. I keep
-        dedicating myself and studying more and more. Always looking to be
-        better than yesterday.
+        Donec bibendum quam interdum ex lacinia blandit. Nullam ac bibendum
+        metus, ac porta tortor. Mauris tristique ante non commodo facilisis.
+        Aliquam lacinia sed orci in malesuada. Duis in venenatis odio. In ac
+        enim ut ipsum tempus lacinia. Vestibulum euismod commodo risus id
+        bibendum. Mauris vel ex auctor, dignissim tortor eu, eleifend massa.
+        Pellentesque porttitor turpis ex, eget sagittis ligula aliquam in. Nam
+        ultrices diam quis enim consequat, nec rutrum velit bibendum. Phasellus
+        quis odio a tortor viverra mollis vitae non dolor.
       </p>
     </Layout>
   );
