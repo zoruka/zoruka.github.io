470d53a89bb198899790abc0636b95fa7ce8307c
Felipe Mendes <zo.fmendes@gmail.com>
2021-08-04 21:42:06 -0300

    :sparkles: add new skills

--- src/compositions/skills.tsx
+++ src/compositions/skills.tsx
@@ -29,6 +29,10 @@ const CardFace = styled.div`
   li {
     padding: 4px 0;
   }
+
+  .card-title {
+    margin: 10px;
+  }
 `;
 
 const CardsGrid = styled.div`
@@ -51,22 +55,40 @@ const CardsGrid = styled.div`
 
 const SkillCards = [
   {
+    icon: 'NodeJS',
+    name: 'Node.js',
+    description:
+      'A lot of daily problems solver and automation projects developed.\nKnowledge in many different frameworks based on Node.js such as Express.js, React, Angular, Ionic.\nDeveloped one big application that is used by thousands of users.',
+  },
+  {
     icon: 'ReactJS',
     name: 'React.js',
     description:
       'Full projects developed.\nMain concepts learned pratically with production projects from scratch to deploy.\nComplete knowledge of Hooks usage.\nExperience in many React libraries such as styled-components, redux and material-ui.',
   },
   {
-    icon: 'NodeJS',
-    name: 'Node.js',
-    description:
-      'A lot of daily problems solver and automation projects developed.\nKnowledge in many different frameworks based on Node.js such as Express.js, React, Angular, Ionic.\nKnowledge in unit testing with Jest.js.',
-  },
-  {
     icon: 'Typescript',
     name: 'Typescript',
     description:
       'Full projects developed from scratch to deploy.\nComplete knowledge of building advanced types.\nProjects developed with a lot of different Node.js based frameworks.',
+  },
+  {
+    icon: 'Testing',
+    name: 'Unit Tests',
+    description:
+      'Able to build modular testable architectures.\nHigh knowledge in Jest.js use.\nThousands of unit tests done.',
+  },
+  {
+    icon: 'Database',
+    name: 'SQL and noSQL',
+    description:
+      'Full projects developed using MongoDB and CouchDB.\nKnowledge in SQL architecture and query building, experience in MySQL and PostgreSQL.\nParticipation in one SQL to noSQL database conversion.',
+  },
+  {
+    icon: 'CloudDevelop',
+    name: 'Cloud Development',
+    description:
+      'Knowledge in more usual AWS features.\nKnowledge in Docker use.\nAlready made full deploys of web applications.\nMeasurement and analysis of production servers.',
   },
 ];
 
@@ -83,7 +105,7 @@ export const SkillsComposition: React.FC = () => {
           <FlipCard className="flip-card">
             <CardFace>
               {Icons[icon]}
-              {name}
+              <span className="card-title">{name}</span>
             </CardFace>
             <CardFace>
               <p>
