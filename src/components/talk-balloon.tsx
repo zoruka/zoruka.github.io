b9b9564c2703a642c0e9f43de0774bbc943008a1
Felipe Mendes <zo.fmendes@gmail.com>
2024-05-16 08:25:45 +0900

    feat: add animation for hello world message

--- src/components/talk-balloon.tsx
+++ src/components/talk-balloon.tsx
@@ -1,65 +1,84 @@
 'use client';
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
-import styles from './talk-balloon.module.css';
+import styles from './talk-balloon.module.scss';
 
 export const TalkBalloon: React.FC<{
-	children: React.ReactNode;
+  children: string;
 }> = ({ children }) => {
-	return (
-		<div className={styles.container}>
-			{children}
-			<AnimatedStroke />
-		</div>
-	);
+  const [text, setText] = useState(children[0]);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      if (text.length < children.length) {
+        setText(children.slice(0, text.length + 1));
+      }
+    }, 250 - Math.random() * 210);
+
+    return () => clearTimeout(timeout);
+  }, [text]);
+
+  return (
+    <span className={styles.container}>
+      {text.length === children.length ? (
+        <>
+          {text.slice(0, -1)}
+          <span className={styles.blinking}>{text.slice(-1)}</span>
+        </>
+      ) : (
+        text
+      )}
+      <AnimatedStroke />
+    </span>
+  );
 };
 
 const AnimatedStroke: React.FC = () => {
-	return (
-		<StrokeContainer>
-			<StrokeLine orientation="horizontal-top" />
-			<StrokeLine orientation="horizontal-bottom" />
-			<StrokeLine orientation="vertical-right" />
-			<StrokeLine orientation="vertical-left" />
-		</StrokeContainer>
-	);
+  return (
+    <StrokeContainer>
+      <StrokeLine orientation="horizontal-top" />
+      <StrokeLine orientation="horizontal-bottom" />
+      <StrokeLine orientation="vertical-right" />
+      <StrokeLine orientation="vertical-left" />
+    </StrokeContainer>
+  );
 };
 
 const StrokeContainer = styled.div`
-	position: absolute;
-	overflow: hidden;
+  position: absolute;
+  overflow: hidden;
 
-	width: calc(100% - 16px);
-	height: calc(100% - 16px);
-	left: 7px;
-	top: 7px;
-	border-radius: 5px;
+  width: calc(100% - 16px);
+  height: calc(100% - 16px);
+  left: 7px;
+  top: 7px;
+  border-radius: 5px;
 `;
 
 const StrokeLine = styled.div<{ orientation: string }>`
-	position: absolute;
-	border-style: dashed;
-	border-color: var(--blue);
+  position: absolute;
+  border-style: dashed;
+  border-color: var(--blue);
 
-	width: ${(props) =>
-		props.orientation.includes('horizontal') ? '1000px' : '0px'};
+  width: ${(props) =>
+    props.orientation.includes('horizontal') ? '1000px' : '0px'};
 
-	height: ${(props) =>
-		props.orientation.includes('horizontal') ? '0px' : '1000px'};
+  height: ${(props) =>
+    props.orientation.includes('horizontal') ? '0px' : '1000px'};
 
-	border-left-width: ${(props) =>
-		props.orientation.includes('left') ? '1px' : '0px'};
-	border-top-width: ${(props) =>
-		props.orientation.includes('top') ? '1px' : '0px'};
-	border-bottom-width: ${(props) =>
-		props.orientation.includes('bottom') ? '1px' : '0px'};
-	border-right-width: ${(props) =>
-		props.orientation.includes('right') ? '1px' : '0px'};
+  border-left-width: ${(props) =>
+    props.orientation.includes('left') ? '1px' : '0px'};
+  border-top-width: ${(props) =>
+    props.orientation.includes('top') ? '1px' : '0px'};
+  border-bottom-width: ${(props) =>
+    props.orientation.includes('bottom') ? '1px' : '0px'};
+  border-right-width: ${(props) =>
+    props.orientation.includes('right') ? '1px' : '0px'};
 
-	${(props) =>
-		props.orientation.includes('top') &&
-		`
+  ${(props) =>
+    props.orientation.includes('top') &&
+    `
     top: 0;
     animation: stroke-top-transition 30s 0s infinite linear;
     @keyframes stroke-top-transition {
@@ -72,9 +91,9 @@ const StrokeLine = styled.div<{ orientation: string }>`
     }
   `}
 
-	${(props) =>
-		props.orientation.includes('bottom') &&
-		`
+  ${(props) =>
+    props.orientation.includes('bottom') &&
+    `
     bottom: 0;
     animation: stroke-bottom-transition 30s 0s infinite linear;
     @keyframes stroke-bottom-transition {
@@ -88,8 +107,8 @@ const StrokeLine = styled.div<{ orientation: string }>`
   `}
 
   ${(props) =>
-		props.orientation.includes('left') &&
-		`
+    props.orientation.includes('left') &&
+    `
     left: 0;
     animation: stroke-left-transition 30s 0s infinite linear;
     @keyframes stroke-left-transition {
@@ -103,8 +122,8 @@ const StrokeLine = styled.div<{ orientation: string }>`
   `}
 
   ${(props) =>
-		props.orientation.includes('right') &&
-		`
+    props.orientation.includes('right') &&
+    `
     right: 0;
     animation: stroke-right-transition 30s 0s infinite linear;
     @keyframes stroke-right-transition {
