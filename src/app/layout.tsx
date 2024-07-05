8636ea6ba4bfccbb47b7c22dd580dc0cd9ed44a5
Felipe Mendes <zo.fmendes@gmail.com>
2024-07-05 12:56:29 -0300

    refactor: update fonts styling

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,25 @@
 import { FathomAnalytics } from '@/components';
 import './globals.css';
 import type { Metadata } from 'next';
-import { Press_Start_2P } from 'next/font/google';
+import { Press_Start_2P, DM_Sans, DM_Mono } from 'next/font/google';
+import { cn } from '@/utils';
 
 const pressStart = Press_Start_2P({
   weight: '400',
   subsets: ['latin-ext'],
+  variable: '--font-press-start',
+});
+
+const dmSans = DM_Sans({
+  weight: ['200', '500', '700'],
+  subsets: ['latin-ext'],
+  variable: '--font-sans',
+});
+
+const dmMono = DM_Mono({
+  weight: ['400'],
+  subsets: ['latin-ext'],
+  variable: '--font-mono',
 });
 
 export const metadata: Metadata = {
@@ -26,7 +40,11 @@ export default function RootLayout({
           includedDomains={['zoruka.xyz', 'www.zoruka.xyz']}
         />
       </head>
-      <body className={pressStart.className}>{children}</body>
+      <body
+        className={cn(pressStart.variable, dmSans.variable, dmMono.variable)}
+      >
+        {children}
+      </body>
     </html>
   );
 }
